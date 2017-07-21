3b3e875997993edf7e377accc0fe67feaa3027b8
Mihir Shah <mihirdshah101@gmail.com>
2017-07-21 15:26:59 -0700

    Initial commit

---
+++ server/configure.js
@@ -0,0 +1,35 @@
+var path = require('path'),
+	routes = require('./routes'),
+	exphbs = require('express-handlebars'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	morgan = require('morgan'),
+	methodOverride = require('method-override'),
+	errorhandler = require('errorhandler');
+
+
+module.exports = function(app){
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({'extended':true}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+	app.use(cookieParser('some-secret-value-here'));
+	routes(app); //moving the routes to routed folder
+	app.use('/public/',express.static(path.join(__dirname,'../public')));
+
+	if('development' === app.get('env')){
+		app.use(errorhandler());
+	}
+
+	app.engine('handlebars',exphbs.create({
+
+		defaultLayout: 'main',
+		layoutsDir: app.get('views') + '/layouts',
+		partialsDir: [app.get('views') + '/partials']
+	}).engine);
+	app.set('view engine', 'handlebars');
+
+		return app;
+
+};
